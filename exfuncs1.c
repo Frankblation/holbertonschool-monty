3765ad0010dd27b490f6d504fef40fcaf6e68c59
Frankblation <frankblation@gmail.com>
2023-08-14 18:55:05 -0700

    3rd monty

---
+++ exfuncs1.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+/**
+ * new_Node - create new node
+ * @n: is a value
+ * Return: new node
+ */
+stack_t *new_Node(int n)
+{
+	stack_t *new = NULL;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
+}
+/**
+ * free_dlistint - Free a list.
+ * @stack: Head node.
+ * Return: Nothing.
+ */
+void free_dlistint(stack_t *stack)
+{
+	stack_t *current = NULL;
+
+	current = stack;
+
+	if (current != NULL)
+	{
+		free_dlistint(current->next);
+		free(current);
+	}
+}
--- func1.c
+++ func1.c
@@ -1,28 +1,5 @@
 #include "monty.h"
-
 int value;
-/**
- * new_Node - create new node
- * @n: is a value
- * Return: new node
- */
-stack_t *new_Node(int n)
-{
-	stack_t *new = NULL;
-
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
-
-	return (new);
-}
-
 /**
  * _push - push item
  * @stack: is a parameter
@@ -40,7 +17,6 @@ void _push(stack_t **stack, unsigned int line_number)
 		(*stack)->prev = new;
 	*stack = new;
 }
-
 /**
  * _pall - print elements stack
  * @stack: is a parameter
@@ -58,24 +34,6 @@ void _pall(stack_t **stack, unsigned int n)
 	{
 		dprintf(STDOUT_FILENO, "%d\n", current->n);
 		current = current->next;
-	}
-}
-
-/**
- * free_dlistint - Free a list.
- * @stack: Head node.
- * Return: Nothing.
- */
-void free_dlistint(stack_t *stack)
-{
-	stack_t *current = NULL;
-
-	current = stack;
-
-	if (current != NULL)
-	{
-		free_dlistint(current->next);
-		free(current);
 	}
 }
 
